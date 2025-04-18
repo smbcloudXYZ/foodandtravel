02d907efb80ba077fed18752c050ab1271ccfd75
Seto Elkahfi <setoelkahfi@gmail.com>
2025-04-18 16:43:00 +0200

    fix deploy

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const buildConfig = () => {
   const blogId = process.env.NEXT_PUBLIC_BLOG_ID;
   if (!blogId) throw new Error("NEXT_PUBLIC_BLOG_ID is missing");
-  const name = process.env.NEXT_PUBLIC_BLOG_DISPLAY_NAME || "Travel.";
+  const name = process.env.NEXT_PUBLIC_BLOG_DISPLAY_NAME || "Food and Travel.";
   const copyright = process.env.NEXT_PUBLIC_BLOG_COPYRIGHT || "Seto Elkahfi";
   const defaultTitle =
     process.env.NEXT_DEFAULT_METADATA_DEFAULT_TITLE || "Travel with Seto Elkahfi";
